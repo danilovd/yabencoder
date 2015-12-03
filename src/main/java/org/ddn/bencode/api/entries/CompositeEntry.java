51d3419067811e0b09106a82afb9f246d3174705
danilovd <dns.msk@gmail.com>
2015-12-03 10:07:16 +0300

    added javadocs to gh-pages

--- src/main/java/org/ddn/bencode/api/entries/CompositeEntry.java
+++
@@ -1,9 +0,0 @@
-package org.ddn.bencode.api.entries;
-
-/**
- * marker interface for B-Encode entries which hold additional values such as dictionary and list
- * @param <T> type of value that entry holds
- */
-public interface CompositeEntry<T> extends Entry<T> {
-
-}
